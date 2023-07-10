97d384527acdd8cf4af666973e682d83ffbc9af9
ljohn <ljohn@mbp2021.local>
2023-07-10 21:52:14 +0800

    修改gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,13 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://run.ljohn.cn',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://www.ljohn.cn',
       },
       {
         name: 'About',
